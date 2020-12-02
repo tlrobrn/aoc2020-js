e3cf039a14fe4f244beeadcaa36b2e607b4fb4b1
Taylor O'Brien <obrien.taylorm@gmail.com>
2020-12-01 22:45:32 -0700

    background and layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import { Router, Link } from "@reach/router";
 import Day1 from "./Day1";
 import Day2 from "./Day2";
+import BackgroundImage from "./BackgroundImage.jpg";
 
 export default function App() {
   return (
-    <Router>
-      <Main path="/" />
-      <Day1 path="/day/1" />
-      <Day2 path="/day/2" />
-      <Day path="/day/:dayId" />
-    </Router>
+    <div
+      className="flex flex-col justify-between h-screen bg-center bg-cover"
+      style={{ backgroundImage: `url(${BackgroundImage})` }}
+    >
+      <header>Advent of Code 2020</header>
+      <article>
+        <Router>
+          <Main path="/" />
+          <Day1 path="/day/1" />
+          <Day2 path="/day/2" />
+          <Day path="/day/:dayId" />
+        </Router>
+      </article>
+      <footer>
+        <span>
+          Photo by{" "}
+          <a href="https://unsplash.com/@chadmadden?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Chad Madden
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/s/photos/christmas?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </span>
+      </footer>
+    </div>
   );
 }
 
@@ -32,7 +53,7 @@ const Main = () => (
 
 const Card = ({ children, dayId }) => (
   <Link to={`/day/${dayId}`}>
-    <div className="p-2 m-2 rounded shadow-md">{children}</div>
+    <div className="p-2 m-2 rounded shadow-md bg-gray-50">{children}</div>
   </Link>
 );
 
