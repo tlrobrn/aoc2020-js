2db86d71f8c5880993137b956229b18253265092
Taylor O'Brien <obrien.taylorm@gmail.com>
2020-12-04 09:45:35 -0700

    day4 part2

--- src/Day4/index.js
+++ src/Day4/index.js
@@ -21,20 +21,22 @@ export default function Day4() {
 }
 
 function Solution() {
-  const { puzzleInput } = usePuzzleInput();
-  const passports = puzzleInput
-    .split(/\n\s*\n/)
-    .map((input) => new Passport(input));
-
-  const result = passports.filter((passport) => passport.isValid()).length;
+  const passports = usePassports();
+  const result = passports.filter((passport) => passport.isComplete()).length;
 
   return <div>Part 1: {result}</div>;
 }
 
 function Solution2() {
-  const { puzzleInput } = usePuzzleInput();
+  const passports = usePassports();
+  const result = passports.filter((passport) => passport.isValid()).length;
 
-  return <div>Part 2: {puzzleInput.length}</div>;
+  return <div>Part 2: {result}</div>;
+}
+
+function usePassports() {
+  const { puzzleInput } = usePuzzleInput();
+  return puzzleInput.split(/\n\s*\n/).map((input) => new Passport(input));
 }
 
 class Passport {
@@ -47,12 +49,116 @@ class Passport {
   parse(input) {
     this.fields = input.split(/\s+/).reduce((fields, entry) => {
       const [tag, value] = entry.split(":");
-      fields.set(tag, value);
+      fields.set(tag, Field.for({ tag, value }));
       return fields;
     }, new Map());
   }
 
-  isValid() {
+  isComplete() {
     return Passport.requiredFields.every((field) => this.fields.has(field));
+  }
+
+  isValid() {
+    return Passport.requiredFields.every(
+      (field) => this.fields.has(field) && this.fields.get(field).isValid()
+    );
+  }
+}
+
+class Field {
+  static for({ tag, value }) {
+    switch (tag) {
+      case "byr":
+        return new ByrField(value);
+      case "iyr":
+        return new IyrField(value);
+      case "eyr":
+        return new EyrField(value);
+      case "hgt":
+        return new HgtField(value);
+      case "hcl":
+        return new HclField(value);
+      case "ecl":
+        return new EclField(value);
+      case "pid":
+        return new PidField(value);
+      default:
+        return new Field(value);
+    }
+  }
+
+  constructor(value) {
+    this.value = value;
+  }
+
+  isValid() {
+    return false;
+  }
+}
+
+class ByrField extends Field {
+  isValid() {
+    const n = parseInt(this.value, 10);
+    return n >= 1920 && n <= 2002;
+  }
+}
+
+class IyrField extends Field {
+  isValid() {
+    const n = parseInt(this.value, 10);
+    return n >= 2010 && n <= 2020;
+  }
+}
+
+class EyrField extends Field {
+  isValid() {
+    const n = parseInt(this.value, 10);
+    return n >= 2020 && n <= 2030;
+  }
+}
+
+class HgtField extends Field {
+  static PARSER = /^(?<inches>\d+)in|(?<centimeters>\d+)cm$/;
+
+  isValid() {
+    const match = HgtField.PARSER.exec(this.value);
+    return match && this.validateMeasurement(match.groups);
+  }
+
+  validateMeasurement({ inches, centimeters }) {
+    if (inches) {
+      const n = parseInt(inches, 10);
+      return n >= 59 && n <= 76;
+    }
+    if (centimeters) {
+      const n = parseInt(centimeters, 10);
+      return n >= 150 && n <= 193;
+    }
+
+    return false;
+  }
+}
+
+class HclField extends Field {
+  static PARSER = /^#[0-9a-fA-F]{6}$/;
+
+  isValid() {
+    return HclField.PARSER.test(this.value);
+  }
+}
+
+class EclField extends Field {
+  static PARSER = /^amb|blu|brn|gry|grn|hzl|oth$/;
+
+  isValid() {
+    return EclField.PARSER.test(this.value);
+  }
+}
+
+class PidField extends Field {
+  static PARSER = /^\d{9}$/;
+
+  isValid() {
+    return PidField.PARSER.test(this.value);
   }
 }
