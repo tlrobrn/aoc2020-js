71a28a4da6d3448c436301cd75f798575d2ffc51
Taylor O'Brien <obrien.taylorm@gmail.com>
2020-12-15 15:59:39 -0700

    refactor to make all ships based on waypoint

--- src/Day12/index.js
+++ src/Day12/index.js
@@ -22,7 +22,7 @@ export default function Day12() {
 
 function Solution() {
   const instructions = useInstructions();
-  const ship = new Ship();
+  const ship = new Ship({ x: 1, y: 0 });
   ship.performManuever(instructions);
   const result = manhattenDistance(ship);
 
@@ -30,7 +30,7 @@ function Solution() {
 }
 
 function Solution2() {
-  const instructions = useWaypointInstructions();
+  const instructions = useInstructions();
   const ship = new WaypointShip({ x: 10, y: 1 });
   ship.performManuever(instructions);
   const result = manhattenDistance(ship);
@@ -41,11 +41,6 @@ function Solution2() {
 function useInstructions() {
   const { puzzleInput } = usePuzzleInput();
   return puzzleInput.split(/\n/).map((line) => Instruction.parse(line));
-}
-
-function useWaypointInstructions() {
-  const { puzzleInput } = usePuzzleInput();
-  return puzzleInput.split(/\n/).map((line) => WaypointInstruction.parse(line));
 }
 
 function manhattenDistance({ position: { x, y } }) {
@@ -86,104 +81,29 @@ class Instruction {
 
 class North extends Instruction {
   apply(ship) {
-    ship.position.y += this.value;
+    ship.applyTo.y += this.value;
   }
 }
 
 class South extends Instruction {
   apply(ship) {
-    ship.position.y -= this.value;
+    ship.applyTo.y -= this.value;
   }
 }
 
 class East extends Instruction {
   apply(ship) {
-    ship.position.x += this.value;
+    ship.applyTo.x += this.value;
   }
 }
 
 class West extends Instruction {
   apply(ship) {
-    ship.position.x -= this.value;
+    ship.applyTo.x -= this.value;
   }
 }
 
 class Left extends Instruction {
-  apply(ship) {
-    ship.heading += this.value;
-  }
-}
-
-class Right extends Instruction {
-  apply(ship) {
-    ship.heading -= this.value;
-  }
-}
-
-class Forward extends Instruction {
-  apply(ship) {
-    ship.position.x += this.value * cos(ship.heading);
-    ship.position.y += this.value * sin(ship.heading);
-  }
-}
-
-class WaypointInstruction {
-  static parse(line) {
-    const [action, value] = [line[0], parseInt(line.slice(1), 10)];
-    switch (action) {
-      case "N":
-        return new WaypointNorth(value);
-      case "S":
-        return new WaypointSouth(value);
-      case "E":
-        return new WaypointEast(value);
-      case "W":
-        return new WaypointWest(value);
-      case "L":
-        return new WaypointLeft(value);
-      case "R":
-        return new WaypointRight(value);
-      case "F":
-        return new WaypointForward(value);
-      default:
-        throw new Error(`invalid action: ${action}`);
-    }
-  }
-
-  constructor(value) {
-    this.value = value;
-  }
-
-  apply() {
-    throw new Error("not implemented");
-  }
-}
-
-class WaypointNorth extends Instruction {
-  apply(ship) {
-    ship.waypoint.y += this.value;
-  }
-}
-
-class WaypointSouth extends Instruction {
-  apply(ship) {
-    ship.waypoint.y -= this.value;
-  }
-}
-
-class WaypointEast extends Instruction {
-  apply(ship) {
-    ship.waypoint.x += this.value;
-  }
-}
-
-class WaypointWest extends Instruction {
-  apply(ship) {
-    ship.waypoint.x -= this.value;
-  }
-}
-
-class WaypointLeft extends Instruction {
   apply(ship) {
     const c = cos(this.value);
     const s = sin(this.value);
@@ -196,7 +116,7 @@ class WaypointLeft extends Instruction {
   }
 }
 
-class WaypointRight extends Instruction {
+class Right extends Instruction {
   apply(ship) {
     const c = cos(this.value);
     const s = sin(this.value);
@@ -209,7 +129,7 @@ class WaypointRight extends Instruction {
   }
 }
 
-class WaypointForward extends Instruction {
+class Forward extends Instruction {
   apply(ship) {
     ship.position.x += this.value * ship.waypoint.x;
     ship.position.y += this.value * ship.waypoint.y;
@@ -226,24 +146,23 @@ function sin(degrees) {
   return Math.round(Math.sin(radians(degrees)));
 }
 
-class Manueverable {
+class Ship {
+  constructor(waypoint) {
+    this.position = { x: 0, y: 0 };
+    this.waypoint = { ...waypoint };
+  }
+
   performManuever(instructions) {
     instructions.forEach((instruction) => instruction.apply(this));
   }
-}
 
-class Ship extends Manueverable {
-  constructor() {
-    super();
-    this.position = { x: 0, y: 0 };
-    this.heading = 0;
+  get applyTo() {
+    return this.position;
   }
 }
 
-class WaypointShip extends Manueverable {
-  constructor(waypoint) {
-    super();
-    this.position = { x: 0, y: 0 };
-    this.waypoint = { ...waypoint };
+class WaypointShip extends Ship {
+  get applyTo() {
+    return this.waypoint;
   }
 }
